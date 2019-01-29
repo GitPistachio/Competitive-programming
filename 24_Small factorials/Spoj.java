087f29630506bca48bb4684c14700bcf88b6a07f
Wojciech Raszka <47065401+GitPistachio@users.noreply.github.com>
2019-01-29 23:04:25 +0100

    Added solution in Java for 24th problem called Small factorials

---
+++ 24_Small factorials/Spoj.java
@@ -0,0 +1,27 @@
+//Solution of 24th problem in SPOJ called Small factorials
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+class Spoj
+{
+  public static BigInteger factorial(int n)
+  {
+    if (n > 1)
+      return factorial(n - 1).multiply(BigInteger.valueOf(n));
+
+    return BigInteger.valueOf(1);
+  }
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int no_of_test_cases;
+
+    no_of_test_cases  = sc.nextInt();
+
+    for (int i=1; i<=no_of_test_cases; i++)
+    {
+      System.out.println(factorial(sc.nextInt()));
+    }
+  }
+}
