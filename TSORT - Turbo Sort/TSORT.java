2b7c6f13c040a4f293975db380f10d9da728f322
Wojciech Raszka <47065401+GitPistachio@users.noreply.github.com>
2019-04-27 07:41:25 +0200

    Optimization of hybrid introspective sort

--- TSORT - Turbo Sort/TSORT.java
+++ TSORT - Turbo Sort/TSORT.java
@@ -4,9 +4,9 @@
 * E-mail       : gitpistachio.gmail.com
 * Date created : 2019-04-24
 * Description  :
-* Status       : Accepted (23681127, 23682129, 23685592)
-* Tags         : java, fast I/O, sorting, bubble sort, optimized bubble sort, insertion sort, selection sort, heap sort, merge sort, median of three quick sort, hybrid introspective quick sort
-* Comment      : builtin sorting function 0.3s, bubble sort TLE, optimized bubble sort TLE, insertion sort TLE, selection sort TLE, heap sort 0.3, merge sort 0.33, median of three quick sort TLE, hybrid introspective quick sort 0.56s
+* Status       : Accepted (23681127, 23682129, 23685592, 23690652)
+* Tags         : java, fast I/O, sorting, bubble sort, optimized bubble sort, insertion sort, selection sort, heap sort, merge sort, median of three quick sort, hybrid introspective quick sort, optimized hybrid introspective quick sort
+* Comment      : builtin sorting function 0.3s, bubble sort TLE, optimized bubble sort TLE, insertion sort TLE, selection sort TLE, heap sort 0.3, merge sort 0.33, median of three quick sort TLE, hybrid introspective quick sort 0.56s, optimized hybrid introspective quick sort 0.31s
 */
 
 import java.lang.StringBuilder;
@@ -291,6 +291,35 @@ final class TSORT{
     insertionSort(A);
   }
 
+  public static boolean optimizedIntroSort(int A[], int l, int r, int max_depth){
+    if (max_depth == 0){
+      heapSort(A);
+      return false;
+    } else if (r - l > 8) {
+        int i = partition(A, l, r);
+        if (introSort(A, l, i - 1, max_depth - 1)){
+            if (introSort(A, i + 1, r, max_depth - 1)){
+              return true;
+            } else {
+              return false;
+            }
+        } else {
+          return false;
+        }
+    }
+    return true;
+  }
+
+  public static void optimizedHybridIntrospectiveSort(int A[]){
+    int n = A.length;
+    int max_depth = (int)Math.floor(Math.log(n))*2;
+
+    if (introSort(A, 0, n - 1, max_depth)){
+        insertionSort(A);
+    }
+
+  }
+
   public static void main(String args[]) throws IOException{
     Reader r = new Reader();
     StringBuilder sb = new StringBuilder();
@@ -309,7 +338,8 @@ final class TSORT{
     //heapSort(A); //0.3s
     //mergeSort(A, 0, n - 1); //0.33s
     //quickSort(A, 0, n - 1); //TLE
-    hybridIntrospectiveSort(A); //0.56s
+    //hybridIntrospectiveSort(A); //0.56s
+    optimizedHybridIntrospectiveSort(A); //0.31s
 
     for(int i =  0; i < n; i++){
       sb.append(A[i]);
