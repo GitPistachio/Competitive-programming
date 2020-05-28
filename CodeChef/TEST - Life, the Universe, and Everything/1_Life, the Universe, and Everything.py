c798aa4d5061eab5277a6bcbf916b38b1e95ac0c
Wojciech Raszka <GitPistachio@gmail.com>
2020-05-28 16:38:03 +0200

    Add one problem from CodeChef website

---
+++ CodeChef/TEST - Life, the Universe, and Everything/1_Life, the Universe, and Everything.py
@@ -0,0 +1,15 @@
+# Project name : CodeChef: TEST - Life, the Universe, and Everything
+# Link         : https://www.codechef.com/problems/TEST
+# Author       : Wojciech Raszka
+# E-mail       : contact@gitpistachio
+# Date created : 2019-11-08
+# Description  :
+# Status       : Accepted (33377910)
+# Tags         : python
+# Comment      :
+
+while True:
+    ans = input()
+    if ans == '42':
+        break
+    print(ans)
