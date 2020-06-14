c98e7a9521eba3111b123532046d4910ce8ea482
Wojciech Raszka <GitPistachio@gmail.com>
2020-06-14 10:46:57 +0200

    Another attempt to solve CONTAIN problem

--- Contests/JUNE20B - June Challenge 2020 Division 2/CONTAIN - The Delicious Cake/CONTAIN - The Delicious Cake.py
+++ Contests/JUNE20B - June Challenge 2020 Division 2/CONTAIN - The Delicious Cake/CONTAIN - The Delicious Cake.py
@@ -5,60 +5,84 @@
 # E-mail       : contact@gitpistachio.site
 # Date created : 2020-06-08
 # Description  :
-# Status       : Unaccepted (???)
-# Tags         : python, convex hull, point in convex polygon
+# Status       : Wrong Answer (34351990)
+# Tags         : python, convex hull, point in a convex polygon
 # Score        : 0
 # Comment      :
 
 from sys import exit, stdin, stdout
 
 
-def cross(o, a, b):
-    return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+class Point:
+    __slots__ = 'x', 'y'
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return '(' + str(self.x) + ', ' + str(self.y) + ')'
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __lt__(self, other):
+        if self.x < other.x:
+            return True
+        elif self.x == other.x and self.y < other.y:
+            return True
+
+        return False
+
+    def __eq__(self, other):
+        return (self.x == other.x and self.y == other.y)
+
+
+def vec(a, b):
+    return Point(b.x - a.x, b.y - a.y)
+
+
+def cross(u, v):
+    return u.x*v.y - u.y*v.x
+
+
+def orient(o, a, b):
+    return cross(vec(o, a), vec(o, b))
+
+
+def sign(x):
+    return -1 if x < 0 else (1 if x > 0 else 0)
 
 
 def getConvexHull(points):
-    if len(points) <= 1:
-        return []
-
-    remaining_points = []
     lower = []
     for p in points:
-        while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
-            if cross(lower[-2], lower[-1], p) != 0:
-                remaining_points.append(lower.pop())
-            else:
-                lower.pop()
+        while len(lower) >= 2 and sign(orient(lower[-2], lower[-1], p)) <= 0:
+            lower.pop()
         lower.append(p)
 
-    rest = []
     upper = []
-    for p in reversed(remaining_points):
-        while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
-            if cross(upper[-2], upper[-1], p) != 0:
-                rest.append(upper.pop())
-            else:
-                upper.pop()
+    for p in reversed(points):
+        while len(upper) >= 2 and sign(orient(upper[-2], upper[-1], p)) <= 0:
+            upper.pop()
         upper.append(p)
 
     return lower[:-1] + upper[:-1]
 
 
-def isInsideConvexHull(x, y, points):
+def isPointStrictlyInsideConvexPolygon(p, points):
     side = None
-    n = len(points)
-    for i in range(n):
-        xi, yi = points[i]
-        xj, yj = points[(i + 1) % n]
-
-        d = (x - xi)*(yj - yi) - (y - yi)*(xj - xi)
-        if d == 0:
+    p1 = points[-1]
+    for p2 in points:
+        so = sign(orient(p1, p2, p))
+        if so != 0:
+            if side is None:
+                side = so
+            elif side != so:
+                return False
+        else:
             return False
-
-        if side is None:
-            side = d > 0
-        elif (d > 0) != side:
-            return False
+        p1 = p2
 
     return True
 
@@ -66,27 +90,30 @@ def isInsideConvexHull(x, y, points):
 no_of_test_cases = int(stdin.readline())
 for _ in range(no_of_test_cases):
     n, q = map(int, stdin.readline().split())
-    points = sorted(set(tuple(map(int, stdin.readline().split())) for _ in range(n)))
+    chiefs_points = sorted([Point(*map(int, stdin.readline().split())) for _ in range(n)])
     for _ in range(q):
-        x, y = map(int, stdin.readline().split())
+        points = chiefs_points.copy()
 
-        remaining_points = points.copy()
-        try:
-            remaining_points.remove((x, y))
-        except Exception:
-            pass
+        candle_point = Point(*map(int, stdin.readline().split()))
+        if candle_point in points:
+            points.remove(candle_point)
 
         cnt = 0
         while True:
-            convex_hull = getConvexHull(remaining_points)
-
+            convex_hull = getConvexHull(points)
             if len(convex_hull) < 3:
                 break
 
-            if not isInsideConvexHull(x, y, convex_hull):
+            if not isPointStrictlyInsideConvexPolygon(candle_point, convex_hull):
                 break
 
-            print(convex_hull)
             cnt += 1
+
+            for p in convex_hull:
+                points.remove(p)
+
+            for p in points.copy():
+                if not isPointStrictlyInsideConvexPolygon(candle_point, convex_hull):
+                    points.remove(p)
 
         stdout.write(str(cnt) + '\n')
